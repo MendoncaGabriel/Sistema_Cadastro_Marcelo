edad2f62b2a519b4a403e995bd5ce5a0a383213c
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-05-04 18:59:01 -0300

    organização de pastas e instalação de react
    instalação de react js, flowbite e tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "flowbite-react";
+import {Component} from "./components/form"
+
+function App() {
+  return (
+    <>
+      <p className="text-red-500">Ola mundo</p>
+      <Button color="blue">Blue</Button>
+     
+      <Component />
+    </>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{ejs,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
