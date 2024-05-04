b06296fb9e539cc214f6654a725b77d2b0c8d780
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-05-04 17:59:31 -0300

    init

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 require('dotenv').config()
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -7,10 +8,16 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-connection.connect((error)=>{
-    if(error) throw error;
-    console.log(`Conectado ao banco de dados: ${process.env.DB_NAME}`)
-});
+connection.connect((error) => {
+    if (error) {
+        console.error('===> TESTE ERROR: Erro ao conectar ao banco de dados:', error);
+    } else {
+        console.log(`===> TESTE: Conectado ao banco de dados: ${process.env.DB_NAME}`);
+    }
 
+    // Feche a conexão após o teste, seja bem-sucedido ou não
+    connection.end();
+    console.log('===> FINISH: de Conecção finalizado')
+});
 
 module.exports = connection; 
