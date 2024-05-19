9987dee6d933e08241a4391e9855af7b2e8e6e24
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-05-19 18:13:53 -0300

    ajustes em responsividade de paginas

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -1,9 +1,24 @@
 //SAIR LOGOUT
 document.getElementById('btnSair').addEventListener('click', () => {
-    fetch('/auth/logout', {
-      method: 'GET'
-    })
-    .then(res => {
-      window.location.reload();
-    })
+  fetch('/auth/logout', {
+    method: 'GET'
+  })
+  .then(res => {
+    window.location.reload();
+  })
 })
+
+const menuMobile = document.getElementById('menuMobile');
+
+function abrirMenu(){
+  menuMobile.classList.replace('hidden', 'block')
+  setTimeout(() => {
+    menuMobile.classList.replace('left-[-100%]', 'left-0')
+  }, 10);
+}
+function fecharMenu(){
+  menuMobile.classList.replace('left-0', 'left-[-100%]')
+  setTimeout(() => {
+    menuMobile.classList.replace('block', 'hidden')
+  }, 300);
+}
