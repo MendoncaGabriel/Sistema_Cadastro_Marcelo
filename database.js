1d43642ec105e7abe2faf25917565f97fc2a04d1
Seu Nome <MendoncaGabriel1997@gmail.com>
2024-05-25 09:09:09 -0400

    add pool

--- database.js
+++ database.js
@@ -1,19 +1,37 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10, // Número máximo de conexões no pool
+    queueLimit: 0
 });
 
-connection.connect((error) => {
+pool.getConnection((error, connection) => {
     if (error) {
-        console.error('===> ERROR: Erro ao conectar ao banco de dados:', error);
-    } else {
-        console.log(`===> CONECTADO AO BANCO DE DADOS: ${process.env.DB_NAME}`);
-    };
+        if (error.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('===> ERROR: Conexão com o banco de dados foi perdida.');
+        }
+        if (error.code === 'ER_CON_COUNT_ERROR') {
+            console.error('===> ERROR: Banco de dados tem muitas conexões.');
+        }
+        if (error.code === 'ECONNREFUSED') {
+            console.error('===> ERROR: Conexão com o banco de dados foi recusada.');
+        }
+    }
+
+    if (connection) connection.release(); // Libera a conexão de volta para o pool
+
+    console.log(`===> CONECTADO AO BANCO DE DADOS: ${process.env.DB_NAME}`);
+    return;
 });
 
-module.exports = connection; 
+pool.on('release', (connection) => {
+    console.log('===> Conexão %d liberada', connection.threadId);
+});
+
+module.exports = pool;
